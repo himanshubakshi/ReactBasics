6be47e99f758e3ab025057ec6d31edee600cc179
Bakshi, Himanshu <himanshu_bakshi@intuit.com>
2018-03-07 09:47:27 +0530

    props, children

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,40 @@
 console.log("Yay, the setup works!!")
 
 import React from "react";
-
 import { render } from "react-dom";
- 
+
+import { Header } from "./components/Header"
+import { Home } from "./components/Home"
+
 
  class App extends React.Component{
  	render() {
+
+ 		var user = {
+ 			name:"Bakshi",
+ 			hobbies: ["laughing", "loving", "living"]
+ 		};
+
+
  		return (
+
+
  			<div className="container">
  				<div className="row">
  					<div className="col-xs-10 col-xs-offset-1">
-			 			<h1>
-			 				Hello World!
-						</h1>
+			 			<Header />
+					</div>
+				</div>
+				<div className="row">
+ 					<div className="col-xs-10 col-xs-offset-1">
+			 			<Home name={"Himanshu"} age={28} user={user} >
+			 				<p id="childParagraph">This is a paragraph</p>
+			 				<p id="childParagraph2">This is a paragraph1</p>
+			 				<p id="childParagraph3">This is a paragraph2</p>
+
+			 			</Home>
 
  					</div>
-
 				</div>
 			</div>
 		);
