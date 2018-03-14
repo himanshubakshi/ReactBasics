15427ea8c7a56430ff1efb4637e25dd46a206f27
Bakshi, Himanshu <himanshu_bakshi@intuit.com>
2018-03-14 08:17:44 +0530

    change state from another component2

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,34 +1,32 @@
 import React from "react";
 
-export class Header extends React.Component{
+export const Header = (props) => {
 
-	render(){
-		return(
-			// <nav classNameName="navbar navbar-default">
-			// 	<div classNameName="container">
-			// 		<div classNameName="navbar-header">
-			// 			<ul classNameName="nav navbar-nav">
-			// 				<li>
-			// 					<a href="#">Home</a>
-			// 				</li>
-			// 			</ul>
-			// 		</div>
-			// 	</div>
-			// </nav>
+	return(
+		// <nav classNameName="navbar navbar-default">
+		// 	<div classNameName="container">
+		// 		<div classNameName="navbar-header">
+		// 			<ul classNameName="nav navbar-nav">
+		// 				<li>
+		// 					<a href="#">Home</a>
+		// 				</li>
+		// 			</ul>
+		// 		</div>
+		// 	</div>
+		// </nav>
 
-			<nav className="navbar navbar-default">
-			  <div className="container-fluid">
-			    <div className="navbar-header">
-			      <a className="navbar-brand" href="#">WebSiteName</a>
-			    </div>
-			    <ul className="nav navbar-nav">
-			      <li className="active"><a href="#">{this.props.homeLink}</a></li>
-			      <li><a href="#">Page 1</a></li>
-			      <li><a href="#">Page 2</a></li>
-			      <li><a href="#">Page 3</a></li>
-			    </ul>
-			  </div>
-			</nav>
-		);
-	}
-}
+		<nav className="navbar navbar-default">
+		  <div className="container-fluid">
+		    <div className="navbar-header">
+		      <a className="navbar-brand" href="#">WebSiteName</a>
+		    </div>
+		    <ul className="nav navbar-nav">
+		      <li className="active"><a href="#">{props.homeLink}</a></li>
+		      <li><a href="#">Page 1</a></li>
+		      <li><a href="#">Page 2</a></li>
+		      <li><a href="#">Page 3</a></li>
+		    </ul>
+		  </div>
+		</nav>
+	);
+};
